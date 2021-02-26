cd0da3885d22ceb4efab7c0d6c92c74585a9d3d1
ludah <ludovico.ghergo@gmail.com>
2021-02-26 17:58:50 +0100

    cambiamenti frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,6 +16,13 @@
       <v-btn text @mousedown="goTo('/Shop')">
         <span class="mr-2">Shop</span>
       </v-btn>
+      <GoogleLogin
+        :params="params"
+        :onSuccess="onSuccess"
+        :onFailure="onFailure"
+        :renderParams="renderParams"
+        >Login</GoogleLogin
+      >
     </v-app-bar>
 
     <v-main>
@@ -25,17 +32,59 @@
 </template>
 
 <script>
+import GoogleLogin from "vue-google-login";
+import { LoaderPlugin } from "vue-google-login";
+import Vue from "vue";
+Vue.use(LoaderPlugin, {
+  client_id:
+    "566387838712-mkstlbr8qaa6md7c9d5lq5oe0n9uq2hs.apps.googleusercontent.com",
+});
+Vue.GoogleAuth.then((auth2) => {
+  console.log(auth2.isSignedIn.get());
+  console.log(auth2.currentUser.get());
+});
 export default {
   name: "App",
   methods: {
+    onSuccess(googleUser) {
+      var googlelogin = googleUser.getBasicProfile();
+      axios
+        .get("http://localhost:8082/api/queries/" + vm.id_number)
+        .then(function (response) {
+          vm.question = response.data;
+        })
+        .catch((error) => {
+          if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log("Error", error.message);
+          }
+        });
+
+      googleUser.getBasicProfile();
+    },
+    onFailure() {
+      console.log("failed");
+    },
     goTo(address) {
       this.$router.push(address);
-    }
+    },
   },
-  components: {},
+  components: { GoogleLogin },
 
   data: () => ({
-    //
-  })
+    params: {
+      client_id:
+        "566387838712-mkstlbr8qaa6md7c9d5lq5oe0n9uq2hs.apps.googleusercontent.com",
+    },
+    renderParams: {
+      width: 50,
+      height: 50,
+    },
+  }),
 };
 </script>
